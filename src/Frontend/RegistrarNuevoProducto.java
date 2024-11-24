66944aa74c6a3ce5a4dc91ef11fd693f9cf354a3
IAN <psanthiagofabian@gmail.com>
2024-11-23 21:57:30 -0600

    GUARDADO EXITOSO BD REGISTRO PRODUCTO INVENTARIO

--- src/Frontend/RegistrarNuevoProducto.java
+++ src/Frontend/RegistrarNuevoProducto.java
@@ -60,7 +60,6 @@ public class RegistrarNuevoProducto extends javax.swing.JFrame {
         }
     }
     
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
