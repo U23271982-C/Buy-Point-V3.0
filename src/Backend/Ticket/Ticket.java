b29e7a17f26c0eaf24d507c4430f3b87fb8aa257
= <u23271982@utp.edu.pe>
2024-11-28 16:36:46 -0500

    - Implementación de cabeacera por tipo de Cliente

--- src/Backend/Ticket/Ticket.java
+++ src/Backend/Ticket/Ticket.java
@@ -5,9 +5,12 @@ import java.awt.print.PageFormat;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Ticket implements Printable {
-    private String encabezadoTicket = """
+    private String encabezadoTicketDirecto = """
             COND. LOS PARQUES DE SAN GABRIEL
             Chiclayo - Chiclayo - Lambayeque
             --------------------------------
@@ -15,11 +18,64 @@ public class Ticket implements Printable {
                         Pedido %d
             --------------------------------
             Fecha: %s
-            Cliente: %
             Tipo Pago: %s
             --------------------------------
             Descrp.     Cant.   SubT.  Total
             """;
+    private String encabezadoTicketDelivery = """
+            COND. LOS PARQUES DE SAN GABRIEL
+            Chiclayo - Chiclayo - Lambayeque
+            --------------------------------
+                    RECIBO DE VENTA
+                        Pedido %d
+            --------------------------------
+            Fecha: %s
+            Torre: %d
+            Departamento: %d
+            Tipo Pago: %s
+            --------------------------------
+            Descrp.     Cant.   SubT.  Total
+            """;
+    private String encabezadoTicketCliente = """
+            COND. LOS PARQUES DE SAN GABRIEL
+            Chiclayo - Chiclayo - Lambayeque
+            --------------------------------
+                    RECIBO DE VENTA
+                        Pedido %d
+            --------------------------------
+            Fecha: %s
+            Tipo Pago: %s
+            Cliente:
+                Nombre:
+                Apellidos:
+                DNI:
+            --------------------------------
+            Descrp.     Cant.   SubT.  Total
+            """;
+    private String encabezadoTicketClienteDepartamento = """
+            COND. LOS PARQUES DE SAN GABRIEL
+            Chiclayo - Chiclayo - Lambayeque
+            --------------------------------
+                    RECIBO DE VENTA
+                        Pedido %d
+            --------------------------------
+            Fecha: %s
+            Tipo Pago: %s
+            Cliente:
+                Nombre:
+                Apellidos:
+                DNI:
+                Torre: %d
+                Departamento: %d
+            --------------------------------
+            Descrp.     Cant.   SubT.  Total
+            """;
+    private List<String> tipoEncabezadosTickets = new ArrayList<>(Arrays.asList(
+            encabezadoTicketDirecto,
+            encabezadoTicketDelivery,
+            encabezadoTicketCliente,
+            encabezadoTicketClienteDepartamento
+    ));
     private String lineaTicket = """
             %s
                           %d  s./%.2f s/.%.2f
@@ -29,13 +85,54 @@ public class Ticket implements Printable {
             I.G.V. (18%)             s/. %.2f
             Total:                   s/. %.2f
             """;
-
-    private final String cuerpoTicket =
-            String.format("%s\n%s\n%s", encabezadoTicket, lineaTicket, finalTicket);
+    private String cuerpoTicket;
     private Font fuente;
     private Color colorFuente;
 
     public Ticket() {
+    }
+
+    //#region Getters and Setters
+
+    public List<String> getTipoEncabezadosTickets() {
+        return tipoEncabezadosTickets;
+    }
+
+    public void setTipoEncabezadosTickets(List<String> tipoEncabezadosTickets) {
+        this.tipoEncabezadosTickets = tipoEncabezadosTickets;
+    }
+
+    public String getLineaTicket() {
+        return lineaTicket;
+    }
+
+    public void setLineaTicket(String lineaTicket) {
+        this.lineaTicket = lineaTicket;
+    }
+
+    public String getFinalTicket() {
+        return finalTicket;
+    }
+
+    public void setFinalTicket(String finalTicket) {
+        this.finalTicket = finalTicket;
+    }
+
+    public String getCuerpoTicket() {
+        return cuerpoTicket;
+    }
+
+    public void setCuerpoTicket(String cuerpoTicket) {
+        this.cuerpoTicket = cuerpoTicket;
+    }
+
+    //#endregion
+
+    public String formateadorCuerpoTicket(int idxEncabezado){
+        return this.cuerpoTicket = String.format("%s\n%s\n%s",
+                tipoEncabezadosTickets.get(idxEncabezado),
+                lineaTicket,
+                finalTicket);
     }
 
     public void imprimir() {
