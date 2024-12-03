4168724b608fcbe7b3c40a6e84e6a4a90c1b0385
= <u23271982@utp.edu.pe>
2024-12-03 18:22:41 -0500

    Implementado registrarDetalleVenta

--- src/Backend/Controladores/CtrlDetalleVenta.java
+++ src/Backend/Controladores/CtrlDetalleVenta.java
@@ -1,19 +1,52 @@
 package Backend.Controladores;
 
+import Backend.ConexionBD.SQLServerBD;
 import Backend.Entidades.DetalleVenta;
 import Backend.Gestores.GestorSQLServer;
-
+import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import java.sql.CallableStatement;
 
 public class CtrlDetalleVenta implements GestorSQLServer<DetalleVenta> {
-    ArrayList<DetalleVenta> detallesVenta1;
+    //ArrayList<DetalleVenta> detallesVenta1;
 
-    public CtrlDetalleVenta(ArrayList<DetalleVenta> detallesVenta) {
-        this.detallesVenta1 = detallesVenta;
+    public CtrlDetalleVenta() {
+        
     }
     @Override
     public void registrar(DetalleVenta nuevaEntidad) {
+         String consultaSQL =
+                "{ CALL paT_registrarDetalleVenta" +
+                        "(?, ?, ?, ?, ?) }";
 
+        try (CallableStatement comando =
+                     SQLServerBD.instanciaConexcion()
+                             .conectar().prepareCall(consultaSQL)){
+
+            comando.setInt
+                    (1, nuevaEntidad.getCantidad());
+            comando.setBigDecimal
+                    (2, nuevaEntidad.getPrecioUnitario());
+            comando.setBigDecimal
+                    (3, nuevaEntidad.getSubTotal());
+            comando.setBigDecimal
+                    (4, nuevaEntidad.getTotal());
+            comando.setString
+                    (5, nuevaEntidad.getProducto().getNombreProducto());
+
+            comando.executeUpdate();
+            String aviso = "Se registró la Venta";
+
+            System.out.printf(aviso);
+            JOptionPane.showMessageDialog(null,aviso);
+        } catch (SQLException e) {
+            //throw new RuntimeException(e);
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null,
+                            ("ERROR al registrar la Venta"));
+        }
     }
 
     @Override
