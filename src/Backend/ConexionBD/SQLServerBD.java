8e14c8b9b3849c8edca75b8a631ff7d9159686f5
jhonerit <jhonerit.sandro@gmail.com>
2025-05-06 23:17:23 -0500

    rama dedicada a la encriptacion de datos de conexion

--- src/Backend/ConexionBD/SQLServerBD.java
+++ src/Backend/ConexionBD/SQLServerBD.java
@@ -14,6 +14,7 @@ public class SQLServerBD {
     private Connection conn = null;
     private static SQLServerBD instancia = null;
 
+    //Encriptar datos
     //Patrón Singleton
     public SQLServerBD() {
         final String nombreBD = "BD_BuyPoint";
